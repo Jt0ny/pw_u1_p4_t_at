278f409630dcf8353eab85e88a5d1a528d66f5b6
Anthony T <jtonytipan@gmail.com>
2023-05-13 22:24:26 -0500

    Cambio del branch

--- src/main/java/com/example/demo/PwU1P4TAtApplication.java
+++ src/main/java/com/example/demo/PwU1P4TAtApplication.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.util.StatusPrinter;
+
 @SpringBootApplication
 public class PwU1P4TAtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PwU1P4TAtApplication.class, args);
+		System.out.println("Tarea 3 -- Anthony Tipan");
 	}
 
 }
